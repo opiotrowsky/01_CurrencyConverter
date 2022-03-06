1be72a893e9fdc9ea163306b33bcd4891a8dd498
opiotrowsky <piotrowsky.o@gmail.com>
2022-03-06 20:38:27 +0100

    Currency exchange function implemented.

--- currencyConverter.hpp
+++ currencyConverter.hpp
@@ -1,18 +1,23 @@
+#pragma once
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 
 class Currency{
     double value_;
     std::string currency_;
 
 public:
-    Currency() { std::cout << "Currency constructor.\n"; }
+    Currency() {}
     Currency(double value, std::string currency)
         : value_(value)
         , currency_(currency)
         {}
-    ~Currency() { std::cout << "Currency destructor.\n"; }
+    ~Currency() {}
 
-
+    double getValue() { return value_; }
+    std::string getCurrency() { return currency_; }
+    
+    void changeCurrency(std::string currency);
 };
